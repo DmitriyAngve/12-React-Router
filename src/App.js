7d3cb08fc89b8bdc83be8c310fbbbee0e5ff5d22
Dmitriy Angve <kirirch129@gmail.com>
2023-02-22 14:24:30 +0300

    #

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+
+// const routeDefinition = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinition);
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ProductsPage = () => {
-  return <div>The Products Page</div>;
+  return <h1>The Products Page</h1>;
 };
 
 export default ProductsPage;
