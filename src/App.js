23759d2cd8ac936312cca3a3d8170b7dca1bea5e
Dmitriy Angve <kirirch129@gmail.com>
2023-02-22 14:12:49 +0300

    #

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/Home";
 
-const router = createBrowserRouter({
-  path: "/",
-  element: <HomePage />,
-});
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
 
 function App() {
-  // return <RouterProvider router={router} />;
   return <RouterProvider router={router} />;
 }
 
